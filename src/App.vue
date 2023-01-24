233b3bbda39c8582e807d9a28e2fd4fb791601f5
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-01-24 16:21:51 +0100

    added header

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
 </script>
 
 <template>
-  <figure>
-    <img src="./assets/img/612ce4761b9679000402af1c.png" alt="">
-  </figure>
+  <header>
+    <figure class="d-flex justify-content-center ">
+      <img src="./assets/img/612ce4761b9679000402af1c.png" alt="logo pokemon">
+    </figure>
+  </header>
 </template>
 
 <style lang="scss">
@@ -13,8 +15,6 @@
 
 figure {
   max-width: 100%;
-  display: flex;
-  justify-content: center;
   background-image: url('./assets/img/z7_wYb9-YSvt0pDJlDIu8iuK_aWdqZPg0wvrGdwNvBw.jpg');
   background-position: center;
   position: sticky;
