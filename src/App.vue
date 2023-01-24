3d1a9aca173f5fe5c2a366d72a0ea8644fc715f2
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-01-24 16:19:46 +0100

    added title

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,26 @@
 </script>
 
 <template>
-  <h1 class="text-danger">ciao</h1>
+  <figure>
+    <img src="./assets/img/612ce4761b9679000402af1c.png" alt="">
+  </figure>
 </template>
 
 <style lang="scss">
-@use './assets/scss/style.scss'
+@use './assets/scss/style.scss';
+
+figure {
+  max-width: 100%;
+  display: flex;
+  justify-content: center;
+  background-image: url('./assets/img/z7_wYb9-YSvt0pDJlDIu8iuK_aWdqZPg0wvrGdwNvBw.jpg');
+  background-position: center;
+  position: sticky;
+  top: 0;
+  right: 0;
+
+  img {
+    width: 300px;
+  }
+}
 </style>
